7106e736775362c17d9b6b32123783bcfbe9a5b2
Lan956616 <cd25924218@gmail.com>
2024-04-21 20:20:28 +0800

    use static file & setting express-handlebars

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
 const express = require('express')
 const app = express()
+const {engine} = require('express-handlebars')
 
 const port = 3000
 
+app.engine('.hbs', engine({extname:'.hbs'}))
+app.set('view engine', '.hbs')
+app.set('views', './views')
+
+//取用靜態檔案
+app.use(express.static('public'))
+
 //設定路由
 app.get('/', (req,res)=>{
-  res.send('123')
+  res.render('index')
 })
 
 //監聽伺服器
